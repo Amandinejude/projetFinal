b3e3955b9aaff790f290d83c17a5378f960b18af
Massine <djafer.massine.pro@gmail.com>
2017-02-14 12:36:08 +0100

    Description + Formulaires

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta name="description" content="">
+    <meta name="Cook Mix" content="">
     <meta name="author" content="">
     <title>Cook and mix</title>
     <!-- Bootstrap Core CSS -->
